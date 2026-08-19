0cb6aadd72d5f021ccbd272ef8d29d5c8d484a79
william0927hjk <williekid0718@gmail.com>
2026-08-19 14:12:35 -0700

    Refactor service worker event listeners

--- unblocked-hub/public/sw.js
+++ unblocked-hub/public/sw.js
@@ -1,16 +1,18 @@
 // public/sw.js
 // Scramjet v2 service worker bootstrap.
-// The server serves scramjet's built assets at /scramjet/ from node_modules.
 
 importScripts('/scramjet/scramjet.sw.js');
 
 const scramjet = new ScramjetServiceWorker();
 
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 
-self.addEventListener('fetch', e => {
-  if (scramjet.route(e)) {
-    e.respondWith(scramjet.fetch(e));
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (scramjet.route(event)) {
+    event.respondWith(scramjet.fetch(event));
   }
 });
